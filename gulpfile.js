c4e1d53e51751627c5b969cd4482da307a956d67
Stefan Yohansson <sy.fen0@gmail.com>
2016-03-10 20:26:57 -0300

    baking home and css scheme

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+gulp.task('libs', function() {
+   gulp.src('./node_modules/animate.css/animate.min.css')
+   .pipe(gulp.src('./node_modules/purecss/build/pure-min.css'))
+   .pipe(gulp.dest('./lib'));
+
+   gulp.src('./node_modules/jquery/dist/jquery.min.js')
+   .pipe(gulp.dest('./lib'));
+});
+
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
   .pipe(sass().on('error', sass.logError))
