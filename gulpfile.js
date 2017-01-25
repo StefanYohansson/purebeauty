baef88ac502d12f4a51def4b69089c9aced7e9db
StefanYohansson <sy.fen0@gmail.com>
2017-01-25 17:58:51 -0300

    optimize css and js: assets from ~760 to ~190

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,19 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
 
 gulp.task('libs', function() {
-   gulp.src('./node_modules/animate.css/animate.min.css')
-   .pipe(gulp.src('./node_modules/purecss/build/pure-min.css'))
+   gulp.src('./node_modules/purecss/build/pure-min.css')
    .pipe(gulp.dest('./lib'));
 
-   gulp.src('./node_modules/jquery/dist/jquery.min.js')
+   gulp.src('./node_modules/whatwg-fetch/fetch.js')
+   .pipe(gulp.dest('./lib'));
+
+   gulp.src('./node_modules/promise-polyfill/promise.min.js')
    .pipe(gulp.dest('./lib'));
 
    gulp.src('./node_modules/purecss/build/grids-responsive-min.css')
@@ -23,4 +29,23 @@ gulp.task('sass', function () {
 
 gulp.task('sass:watch', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('minify', function () {
+  gulp.src('./css/*.css')
+    .pipe(cleanCSS({compability: 'ie8'}))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./css/dist'));
+});
+
+gulp.task('compress', function (cb) {
+  pump([
+    gulp.src('lib/main.js'),
+    uglify({ mangle: true, compress: true }),
+    gulp.dest('./lib/dist')
+  ],
+    cb
+  );
 });
